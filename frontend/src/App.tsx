9603edfc45916caa7ee276c1dadba9d64f6e8495
yandersonpark1 <everydayap3@gmail.com>
2025-09-24 23:38:34 -0400

    initial source code

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import UploadForm from "./components/uploadform";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <UploadForm />
+    </div>
+  );
+}
+
+export default App;
