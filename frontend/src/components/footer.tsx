b53da93f33f0b1717ecb5e4ea92cccd326ad667b
yandersonpark1 <everydayap3@gmail.com>
2025-09-27 22:44:26 -0400

    inital footer page

---
+++ frontend/src/components/footer.tsx
@@ -0,0 +1,8 @@
+function Footer() {
+  return (
+    <footer className="bg-white border-t py-4 text-center text-gray-500 text-sm">
+      © {new Date().getFullYear()} Oberlin College Varisty Basball Team. All rights reserved.
+    </footer>
+  );
+}
+export default Footer;
