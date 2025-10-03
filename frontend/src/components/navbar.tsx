db03a5c2e09a35ef539195df4b83ea570862059d
yandersonpark1 <everydayap3@gmail.com>
2025-10-03 15:15:03 -0400

    updated navbar

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,19 +1,12 @@
 export default function Navbar() {
   return (
-    <header className="top-0 bg-white backdrop-blur-md z-50 border-b border-black-200">
+    <header className="fixed top-0 left-0 w-full z-50 bg-black/50 backdrop-blur-md border-b border-blue-800">
       <div className="max-w-6xl mx-auto py-4 px-6 flex items-center justify-between">
-        <div className="flex items-center">
-          <img 
-            src="/oberlin.png" 
-            alt="OC Bsbl" 
-            className="h-12 w-auto mr-2" 
-          />
-        </div>
-
+        <div className="text-3xl font-bold text-white">StatPedia</div>
         <nav className="space-x-10">
-          <a href="#" className="text-black-600 hover:text-blue-600">Home</a>
-          <a href="#" className="text-black-600 hover:text-blue-600">Dashboard</a>
-          <a href="#" className="text-black-600 hover:text-blue-600">About</a>
+          <a href="#" className="text-white hover:text-blue-400">Home</a>
+          <a href="#" className="text-white hover:text-blue-400">Dashboard</a>
+          <a href="#" className="text-white hover:text-blue-400">About</a>
         </nav>
       </div>
     </header>
