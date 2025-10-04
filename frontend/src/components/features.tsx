f94d09607a41b20a743cd23a81f2bdb0607567da
yandersonpark1 <everydayap3@gmail.com>
2025-10-03 23:40:25 -0400

    basic styling updates

--- frontend/src/components/features.tsx
+++ frontend/src/components/features.tsx
@@ -3,15 +3,15 @@ export default function Features() {
     <section className="py-12">
       <div className="max-w-6xl mx-auto px-6 grid md:grid-cols-3 gap-8 text-center">
         {[
-          { title: "Reliability", desc: "Automatically handle messy CSVs" },
-          { title: "Visualization", desc: "Generate visuals from pitch data." },
-          { title: "Automated Reports", desc: "Get results in seconds, not hours." },
+          { title: "Accessibility", desc: "Simply upload your data for insights" },
+          { title: "Visualization", desc: "See areas of growth potential" },
+          { title: "Automated Reports", desc: "Get results instantly" },
         ].map((f, i) => (
           <div
             key={i}
             className="p-10 border-2 border-gold-400 rounded-xl shadow-md bg-white hover:shadow-xl transition"
           >
-            <h3 className="font-bold text-xl text-red-600">{f.title}</h3>
+            <h3 className="font-bold text-xl text-blue-400">{f.title}</h3>
             <p className="mt-2 text-gray-600">{f.desc}</p>
           </div>
         ))}
--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -2,7 +2,12 @@ export default function Navbar() {
   return (
     <header className="fixed top-0 left-0 w-full z-50 bg-black/50 backdrop-blur-md border-b border-blue-800">
       <div className="max-w-6xl mx-auto py-4 px-6 flex items-center justify-between">
-        <div className="text-3xl font-bold text-white">StatPedia</div>
+        <div className="">
+          <span className="text-2xl font-bold tracking-wide bg-gradient-to-r from-blue-300 to-green-300 bg-clip-text text-transparent drop-shadow-lg">
+            Pulse
+          </span>
+
+        </div>
         <nav className="space-x-10">
           <a href="#" className="text-white hover:text-blue-400">Home</a>
           <a href="#" className="text-white hover:text-blue-400">Dashboard</a>
