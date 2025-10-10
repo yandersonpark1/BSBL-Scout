e701beb51fb1ea40af607c0ac5ce376d5cb05208
yandersonpark1 <everydayap3@gmail.com>
2025-10-09 23:12:25 -0400

    updated for pulse

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -2,8 +2,8 @@ export default function Footer() {
   return (
     <footer className="text-gray-200 p-6 text-center">
       <p>
-        © 2025 <span className="text-gold-400 font-semibold">OC Baseball</span>. 
-        Built for baseball lovers.
+        © 2025 <span className="text-gold-400 font-semibold">Pulse Labs Inc. </span>
+        All rights reserved.
       </p>
     </footer>
   );
