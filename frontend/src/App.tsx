3058058d9b8eeea6c4855ce74f6ccdc7ab3000f2
yandersonpark1 <everydayap3@gmail.com>
2025-10-09 23:11:53 -0400

    updated to account for chart components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import UploadPage from "./pages/uploadpage";      // your current landing page
-import InsightsPage from "./pages/insightspage"; // new page for charts
+/// react-router-dom allows for client side eouting 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import UploadPage from "./pages/uploadpage";
+import InsightsPage from "./pages/insightspage"; 
 
+
+/// might have to change Router to browser router when deploying
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<UploadPage />} />
-        <Route path="/insights" element={<InsightsPage />} />
+        <Route path ="/insights/:fileID" element={<InsightsPage />} /> 
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
